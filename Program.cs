ba9ce5214f9032fdfe20a296fb610622f0635dfb
ProgramerPlus <hipitelo@outlook.com>
2025-10-04 16:56:41 -0300

    Exercício finalizado

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("--- Minhas frutas preferidas ---");
+Console.WriteLine("1. Goiaba");
+Console.WriteLine("2. Manga");
+Console.WriteLine("3. Melancia");
+Console.WriteLine("4. Coco");
+Console.WriteLine("5. Abacate");
